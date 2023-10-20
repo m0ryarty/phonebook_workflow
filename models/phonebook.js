cc2d70e5966835b5569f2e08624881f619e4c379
Wilson de Souza Pereira <raskholnikov@gmail.com>
2023-10-20 10:48:34 -0300

    part11 exercise 11.20-10

--- models/phonebook.js
+++ models/phonebook.js
@@ -11,37 +11,37 @@ console.log('url', process.env.MONGODB_URI)
 console.log('connecting to', url)
 
 connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        validate: {
-            validator: function (v) {
-                return v.split('-').length === 2 && /\d{2,3}-\d{5,8}/.test(v)
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        }
-
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return v.split('-').length === 2 && /\d{2,3}-\d{5,8}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     }
+
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 export default model('Person', personSchema)
