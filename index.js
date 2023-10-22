2979f1481ae0f8b01a95b604f730c423a1a76f86
Wilson de Souza Pereira <raskholnikov@gmail.com>
2023-10-21 23:21:53 -0300

    part11 exercise 11.20

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 app.listen((PORT), () => {
   console.log(`Server is running on port ${PORT}`)
 })
